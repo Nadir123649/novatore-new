3c66769090d04cc77e7e8a7b1783e58d4d854133
NovatoreMR <ameer@novatoresols.com>
2024-08-14 21:04:11 +0500

    add changes

--- src/components/CaseStudyCards.tsx
+++ src/components/CaseStudyCards.tsx
@@ -13,12 +13,17 @@ interface CaseStudyProps {
 const CaseStudyCards: React.FC<CaseStudyProps> = ({ banner, backgroundimg, description, logo, color }) => {
     return (
         <div className="study-container relative mr-10 study-box cursor-pointer h-[394px] rounded-2xl max-w-[482px]  w-full transition-all duration-300">
-            <Image src={banner} alt="service" className=' rounded-2xl h-full w-full object-cover' />
+            <Image src={banner} alt="service" className='rounded-2xl h-full w-full object-cover' />
             <div className="overlay" style={{ backgroundImage: `url(${backgroundimg.src})` }}>
-                <div className="text d-flex flex-col  text-2xl  gap-4">
+                <div className="text d-flex flex-col w-full text-2xl gap-4 pr-5 pl-5">
                     <Image src={logo} alt="service" />
-                    <p className='text-2xl h-[170px]'>{description}</p>
-                    <button className='bg-[#FFFFFFCC] rounded-lg px-2 py-1 max-w-[120px]' style={{ color: color }} >Read more</button>
+                    <p className=' text-xl '>{description}</p>
+                    <button
+                        className="bg-white rounded-lg px-4 py-2 max-w-[180px] w-full font-semibold shadow-md transition-all duration-300 ease-in-out transform hover:bg-blue-500 hover:text-white hover:shadow-lg hover:scale-105"
+                        style={{ color: color }}
+                    >
+                        Read more
+                    </button>
                 </div>
             </div>
         </div>
