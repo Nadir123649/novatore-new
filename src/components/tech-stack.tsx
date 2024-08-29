7e5b01a101c3a805b086c94cf9da20b1f96749cd
Nadir123649 <nadir.ali@novatoresols.com>
2024-08-29 20:46:14 +0500

    add changes in css

--- src/components/tech-stack.tsx
+++ src/components/tech-stack.tsx
@@ -13,7 +13,7 @@ const TechStack: React.FC<TechStackProps> = ({ title, images = [] }) => {
                 <h1 className="text-center text-[#313232] text-[40px] font-bold capitalize not-italic leading-[55px] max-w-[590px] mx-auto mb-[40px]">
                     {title}
                 </h1>
-                <div className="flex flex-wrap justify-center mb-[40px] ">
+                <div className="flex flex-wrap justify-center mb-[40px]">
                     {images.map((image, index) => (
                         <Image key={index} src={image} alt={`techstack`} />
                     ))}
