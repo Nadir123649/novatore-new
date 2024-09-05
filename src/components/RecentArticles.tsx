8d4dda9f39f8e2534b26ce108f2767ea35cd9245
Nadir123649 <nadir.ali@novatoresols.com>
2024-09-05 22:48:58 +0500

    add chnages

--- src/components/RecentArticles.tsx
+++ src/components/RecentArticles.tsx
@@ -1,28 +1,36 @@
-import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Container, Row } from 'react-bootstrap';
 import BlogsCard from './BlogsCard';
-import { blogData } from '@/constants/indesx';
+import { client } from "@/sanity/lib/client";
 
 const RecentArticles = () => {
+    const [blogdata, setBlogdata] = useState([]);
+
+    const getBlogData = async () => {
+        const res = await client.fetch("*[_type== 'blogs']");
+        setBlogdata(res);
+    }
+
+    useEffect(() => {
+        getBlogData()
+    }, []);
+
     return (
-        <section  className='bg-[#F6F6F6] pt-16 pb-20 mb-16'>
+        <section className='bg-[#F6F6F6] pt-16 pb-20 mb-16'>
             <Container>
                 <h1 className='text-center text-[#1B232E] text-[40px] font-bold capitalize not-italic leading-[55px] max-w-[590px] mx-auto mb-[80px]'>
                     Read our <span className='text-[#2776EA]'>Recent Articles</span>
                 </h1>
                 <div className='mt-[40px]'>
                     <Row className='gap-y-8'>
-                        {blogData.slice(0, 2).map(blog => (
-                            <Col key={blog.id} lg={6} md={6} xs={12}>
-                                <BlogsCard
-                                    id={blog.id}
-                                    image={blog.image}
-                                    category={blog.category}
-                                    tag={blog.tag}
-                                    title={blog.title}
-                                    link={blog.link}
-                                />
-                            </Col>
+                        {blogdata.map((blog: any) => (
+                            <BlogsCard
+                                key={blog._id}
+                                id={blog._id}
+                                image={blog.image}
+                                subtitle={blog.sub_title}
+                                title={blog.title}
+                            />
                         ))}
                     </Row>
                 </div>
